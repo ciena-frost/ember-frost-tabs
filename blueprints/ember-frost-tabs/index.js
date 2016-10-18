7edfb9d953a407ecd8907040603d23f2e533c5c8
David Fortin <dafortin@ciena.com>
2016-10-18 14:08:19 -0400

    Update dependecies + setup tools

--- blueprints/ember-frost-tabs/index.js
+++ blueprints/ember-frost-tabs/index.js
@@ -11,7 +11,7 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-frost-core', target: '>=0.0.14 <2.0.0'}
+        {name: 'ember-frost-core', target: '^0.29.0'}
       ]
     })
   }
