19792706e539a2be79b544e21ffa07d1ee309add
Matthew Dahl <mdahl@ciena.com>
2017-01-20 17:48:22 -0800

    Update CI config

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,33 +2,173 @@ module.exports = {
   scenarios: [
     {
       name: 'default',
-      dependencies: { }
+      bower: {
+        dependencies: { }
+      }
+    },
+    {
+      name: 'ember-1-12',
+      bower: {
+        dependencies: {
+          'ember': '~1.12.0'
+        },
+        resolutions: {
+          'ember': '~1.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-1-13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-0',
+      bower: {
+        dependencies: {
+          'ember': '~2.0.0'
+        },
+        resolutions: {
+          'ember': '~2.0.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-1',
+      bower: {
+        dependencies: {
+          'ember': '~2.1.0'
+        },
+        resolutions: {
+          'ember': '~2.1.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-2',
+      bower: {
+        dependencies: {
+          'ember': '~2.2.0'
+        },
+        resolutions: {
+          'ember': '~2.2.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-3',
+      bower: {
+        dependencies: {
+          'ember': '~2.3.0'
+        },
+        resolutions: {
+          'ember': '~2.3.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-4',
+      bower: {
+        dependencies: {
+          'ember': '~2.4.0'
+        },
+        resolutions: {
+          'ember': '~2.4.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-5',
+      bower: {
+        dependencies: {
+          'ember': '~2.5.0'
+        },
+        resolutions: {
+          'ember': '~2.5.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-6',
+      bower: {
+        dependencies: {
+          'ember': '~2.6.0'
+        },
+        resolutions: {
+          'ember': '~2.6.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-7',
+      bower: {
+        dependencies: {
+          'ember': '~2.7.0'
+        },
+        resolutions: {
+          'ember': '~2.7.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-8',
+      bower: {
+        dependencies: {
+          'ember': '~2.8.0'
+        },
+        resolutions: {
+          'ember': '~2.8.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2-9',
+      bower: {
+        dependencies: {
+          'ember': '~2.9.0'
+        },
+        resolutions: {
+          'ember': '~2.9.0'
+        }
+      }
     },
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
       }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
       }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     }
   ]
