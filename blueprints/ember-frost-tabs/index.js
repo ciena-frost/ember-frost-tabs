9ada0c98a2b10c206119e429223a356c20e40c76
dafortin@ciena.com <dafortin@ciena.com>
2017-04-18 13:14:22 -0400

    Added blueprint check

--- blueprints/ember-frost-tabs/index.js
+++ blueprints/ember-frost-tabs/index.js
@@ -1,19 +1,30 @@
+const blueprintHelper = require('ember-frost-core/blueprint-helper')
+
 module.exports = {
   description: '',
   normalizeEntityName: function () {},
 
-  /**
-    Installs specified packages at the root level of the application.
-    Triggered by 'ember install <addon name>'.
+  afterInstall: function (options) {
+    const addonsToAdd = [
+      {name: 'ember-frost-core', target: '^1.14.3'},
+      {name: 'ember-simple-uuid', target: '0.1.4'}
+    ]
 
-    @returns {Promise} package names and versions
-  */
-  afterInstall: function () {
-    return this.addAddonsToProject({
-      packages: [
-        {name: 'ember-frost-core', target: '^1.0.3'},
-        {name: 'ember-simple-uuid', target: '0.1.4'}
-      ]
+    // Get the packages installed in the consumer app/addon. Packages that are already installed in the consumer within
+    // the required semver range will not be re-installed or have blueprints re-run.
+    const consumerPackages = blueprintHelper.consumer.getPackages(options)
+
+    // Get the packages to install (not already installed) from a list of potential packages
+    return blueprintHelper.packageHandler.getPkgsToInstall(addonsToAdd, consumerPackages).then((pkgsToInstall) => {
+      if (pkgsToInstall.length !== 0) {
+        // Call the blueprint hook
+        return this.addAddonsToProject({
+          packages: pkgsToInstall,
+          blueprintOptions: {
+            saveDev: true
+          }
+        })
+      }
     })
   }
 }
